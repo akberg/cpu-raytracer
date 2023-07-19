77ca4145206c95eb3ac92e496b4353ad6d6cedcf
Andreas K. Berg <andreaskb98@gmail.com>
2023-07-19 13:22:50 +0200

    Random

--- src/rtweekend.hpp
+++ src/rtweekend.hpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <memory>
 #include <limits>
+#include <cstdlib>
 
 #include <glm/vec3.hpp>
 
@@ -26,4 +27,13 @@ const double pi = 3.1415926535897932385;
 
 inline double degreesToRadians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+/**Return a random float in the [0, 1) */
+inline double random_double() {
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    return min + (max-min)*random_double();
 }
