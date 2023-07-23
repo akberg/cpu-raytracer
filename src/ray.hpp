18fd7c6d277453018c6ca1539ff4c4344bbd1100
Andreas K. Berg <andreaskb98@gmail.com>
2023-07-23 12:16:25 +0200

    m_ prefix private vars in Ray

--- src/ray.hpp
+++ src/ray.hpp
@@ -1,20 +1,22 @@
 #pragma once
 
+#include <glm/glm.hpp>
+
 #include "rtweekend.hpp"
 
 class Ray {
 public:
     Ray() {};
     Ray(const Point& origin, const Vec3& direction)
-        : _origin(origin), _direction(direction) {};
-    Point origin() const { return _origin; };
-    Vec3 direction() const { return _direction; };
+        : m_origin(origin), m_direction(glm::normalize(direction)) {};
+    Point origin() const { return m_direction; };
+    Vec3 direction() const { return m_direction; };
 
     Point at(double t) const {
-        return _origin + t * _direction;
+        return m_origin + t * m_direction;
     }
 
 private:
-    Point _origin;
-    Vec3  _direction;
+    Point m_origin;
+    Vec3  m_direction;
 };
