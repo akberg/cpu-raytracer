e9ffbb60cf8a40e49e232f973440fd8b73e82b5c
Andreas K. Berg <andreaskb98@gmail.com>
2023-07-24 09:20:33 +0200

    gamma

--- src/color.hpp
+++ src/color.hpp
@@ -1,7 +1,8 @@
 #pragma once
-#include<ostream>
 
 #include "rtweekend.hpp"
+
+#include<ostream>
 
 void writeColor(std::ostream& os, Color color) {
     os << static_cast<int>(255.999 * color.r) << ' '
@@ -12,9 +13,11 @@ void writeColor(std::ostream& os, Color color) {
 void writeColor(std::ostream& os, Color color, int samplesPerPixel) {
     auto scale = 1.0 / samplesPerPixel;
 
-    auto r = sqrt(color.r * scale);
-    auto g = sqrt(color.g * scale);
-    auto b = sqrt(color.b * scale);
+    // gamma=2 correction
+    double gamma = 1.25;
+    auto r = std::pow(color.r * scale, 1.0 / gamma);
+    auto g = std::pow(color.g * scale, 1.0 / gamma);
+    auto b = std::pow(color.b * scale, 1.0 / gamma);
 
     os << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
        << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
