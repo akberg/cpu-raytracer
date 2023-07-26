243c915e1200e94871fab923f15166050ddf50a4
Andreas K. Berg <andreaskb98@gmail.com>
2023-07-26 10:32:25 +0200

    add functions on camera

--- src/camera.hpp
+++ src/camera.hpp
@@ -3,10 +3,12 @@
 #include "ray.hpp"
 #include "rtweekend.hpp"
 
+#include <ostream>
+
 class Camera {
 public:
     Camera() : Camera(16.0 / 9.0, 1.0) {}
-    Camera(double aspectRatio, double focalLength) {
+    Camera(double aspectRatio, double focalLength) : aspectRatio(aspectRatio), focalLength(focalLength) {
         auto viewportHeight    = 2.0;
         auto viewportWidth     = aspectRatio * viewportHeight;
 
@@ -19,12 +21,38 @@ public:
             - Vec3(0, 0, focalLength);
     }
 
+    /// @brief Get ray for the UV direction
+    /// @param u Horizontal proportion, in [0, 1)
+    /// @param v Vertical proportion, in [0, 1)
+    /// @return Cast ray
     Ray getRay(double u, double v) const {
         return Ray(origin, lowerLeftCorner + u*horizontal + v*vertical - origin);
     }
 
-private:
+    /// @brief Set aspect ratio. Updates dependent values.
+    /// @param aspect Aspect ratio
+    void setAspectRatio(double aspect) {
+        this->aspectRatio = aspect;
+        horizontal = Point(aspect * vertical.y, 0, 0);
+        lowerLeftCorner  = origin
+            - horizontal / 2.0
+            - vertical / 2.0
+            - Vec3(0, 0, focalLength);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, Camera cam) {
+        os << "Camera(o:"<<cam.origin<<" direction: "<<cam.getRay(0.5, 0.5).direction<<")";
+        return os;
+    }
+
+    /// @brief Origin of lens
     Point origin;
+    /// @brief Focal Lenght affects the spreading angle of cast rays, and
+    /// thereby the field of view. Lower focal length makes a wider angle, and
+    /// a longer focal length makes a narrower angle.
+    double focalLength;
+private:
+    double aspectRatio;
     Point horizontal;
     Point vertical;
     Point lowerLeftCorner;
