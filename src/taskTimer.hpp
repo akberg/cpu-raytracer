fe5a8fd34327ea8172cb51a06a394e93eb919045
Andreas K. Berg <andreaskb98@gmail.com>
2023-10-04 11:12:02 +0200

    Add task timer

---
+++ src/taskTimer.hpp
@@ -0,0 +1,22 @@
+#include <chrono>
+#include <iostream>
+
+class TaskTimer {
+public:
+    void start(std::string msg) {
+        tStart = std::chrono::system_clock::now();
+        std::cerr << msg;
+    }
+    void stop() {
+        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::system_clock::now() - tStart);
+        std::cerr << elapsed << "\n";
+    }
+    void stopRestart(std::string msg) {
+        stop();
+        start(msg);
+    }
+
+private:
+    std::chrono::_V2::system_clock::time_point tStart;
+};
