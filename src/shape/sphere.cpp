3ce3e25032dde0c9791486a71542c9bceec9c5e3
Andreas K. Berg <andreaskb98@gmail.com>
2023-10-10 09:26:35 +0200

    Format

--- src/shape/sphere.cpp
+++ src/shape/sphere.cpp
@@ -1,13 +1,17 @@
 #include "sphere.hpp"
 
-Sphere::Sphere(Point c, double r, shared_ptr<Material> m) : center(c), radius(r), mat(m) {}
+Sphere::Sphere(Point c, double r, shared_ptr<Material> m)
+    : center(c)
+    , radius(r)
+    , mat(m) { }
 
-bool Sphere::hit(const Ray& ray, double tMin, double tMax, HitRecord& rec) const {
-    Vec3 oc = ray.origin - center;
-    auto a = glm::dot(ray.direction, ray.direction);
+bool Sphere::hit(
+    const Ray& ray, double tMin, double tMax, HitRecord& rec) const {
+    Vec3 oc     = ray.origin - center;
+    auto a      = glm::dot(ray.direction, ray.direction);
     auto half_b = glm::dot(oc, ray.direction);
-    auto c = glm::dot(oc, oc) - radius * radius;
-    auto discr = half_b*half_b - a*c;
+    auto c      = glm::dot(oc, oc) - radius * radius;
+    auto discr  = half_b * half_b - a * c;
 
     if (discr < 0) {
         return false;
@@ -26,8 +30,8 @@ bool Sphere::hit(const Ray& ray, double tMin, double tMax, HitRecord& rec) const
 
     // Lots of initialisation, better be a contructor? The Rust way would be
     // returning an Option<HitRecord>
-    rec.t = root;
-    rec.p = ray.at(rec.t);
+    rec.t              = root;
+    rec.p              = ray.at(rec.t);
     Vec3 outwardNormal = (rec.p - center) / radius;
     rec.setFaceNormal(ray, outwardNormal);
     rec.mat = mat;
@@ -38,8 +42,8 @@ bool Sphere::hit(const Ray& ray, double tMin, double tMax, HitRecord& rec) const
 
 void Sphere::getSphereUV(const Vec3& p, double& u, double& v) const {
     auto theta = std::acos(-p.y);
-    auto phi = std::atan2(-p.z, p.x) + pi;
+    auto phi   = std::atan2(-p.z, p.x) + pi;
 
-    u = phi / (2.0*pi);
+    u = phi / (2.0 * pi);
     v = theta / pi;
 }
