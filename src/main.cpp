e4fa62e432b67ed225c0634f4f40dd5f6ca10a09
Andreas K. Berg <andreaskb98@gmail.com>
2023-11-06 10:57:29 +0100

    Add earth texture and quad shape

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "ray.hpp"
 #include "rtweekend.hpp"
 #include "shape/plane.hpp"
+#include "shape/quad.hpp"
 #include "shape/sphere.hpp"
 #include "shape/triangle.hpp"
 #include "taskTimer.hpp"
@@ -42,22 +43,22 @@ HittableList spheres() {
     // World
     HittableList world;
     // Visualise axes:
-    // world.add(make_shared<Sphere>(Point( 0.5, -0.5, -2), 0.2, matLambRed));
-    // world.add(make_shared<Sphere>(Point(-0.5,  0.5, -2), 0.2, matLambGreen));
-    // world.add(make_shared<Sphere>(Point(-0.5, -0.5, -1), 0.2, matLambBlue));
-    // world.add(make_shared<Sphere>(Point(-0.5, -0.5, -2), 0.2, matLambDark));
+    // world.add(make_shared<Sphere>(Vec3( 0.5, -0.5, -2), 0.2, matLambRed));
+    // world.add(make_shared<Sphere>(Vec3(-0.5,  0.5, -2), 0.2, matLambGreen));
+    // world.add(make_shared<Sphere>(Vec3(-0.5, -0.5, -1), 0.2, matLambBlue));
+    // world.add(make_shared<Sphere>(Vec3(-0.5, -0.5, -2), 0.2, matLambDark));
 
-    world.add(make_shared<Sphere>(Point(-1, 0, -2), -0.5, dielectric));
-    world.add(make_shared<Sphere>(Point(1, 0, -2), 0.5, matMetRed));
-    // world.add(make_shared<Sphere>(Point(0, 0, -1), 0.5, matLambRed));
-    world.add(make_shared<Sphere>(Point(0, 0, -2), 0.5, matMetRed2));
+    world.add(make_shared<Sphere>(Vec3(-1, 0, -2), -0.5, dielectric));
+    world.add(make_shared<Sphere>(Vec3(1, 0, -2), 0.5, matMetRed));
+    // world.add(make_shared<Sphere>(Vec3(0, 0, -1), 0.5, matLambRed));
+    world.add(make_shared<Sphere>(Vec3(0, 0, -2), 0.5, matMetRed2));
 
-    world.add(make_shared<Sphere>(Point(10.0, 1.0, -4.0), 1.5, matLambBlue));
-    world.add(make_shared<Sphere>(Point(1.0, 1.0, -6.0), 1.5, matMetRed2));
-    world.add(make_shared<Sphere>(Point(-3.0, 1.0, -5.0), 1.5, matMetLight));
+    world.add(make_shared<Sphere>(Vec3(10.0, 1.0, -4.0), 1.5, matLambBlue));
+    world.add(make_shared<Sphere>(Vec3(1.0, 1.0, -6.0), 1.5, matMetRed2));
+    world.add(make_shared<Sphere>(Vec3(-3.0, 1.0, -5.0), 1.5, matMetLight));
 
-    world.add(make_shared<Sphere>(Point(0, -50.5, -2), 50.0, matMetDark));
-    world.add(make_shared<Plane>(Point(0, -0.5, -4), Vec3(0, 1, 0), matRG));
+    world.add(make_shared<Sphere>(Vec3(0, -50.5, -2), 50.0, matMetDark));
+    world.add(make_shared<Plane>(Vec3(0, -0.5, -4), Vec3(0, 1, 0), matRG));
 
     return world;
 }
@@ -65,24 +66,24 @@ HittableList spheres() {
 HittableList spheresAndTris() {
     HittableList world = spheres();
     world.add(make_shared<Triangle>( // Cw pointing down
-        Point(0.0, 0.2, -1.0),
-        Point(2.0, 1.5, -1.0),
-        Point(0.0, 1.0, -0.9),
+        Vec3(0.0, 0.2, -1.0),
+        Vec3(2.0, 1.5, -1.0),
+        Vec3(0.0, 1.0, -0.9),
         matRG));
     world.add(make_shared<Triangle>( // CW pointing down
-        Point(1.0, 0.1, -1.0),
-        Point(0.5, -0.3, -0.5),
-        Point(0.2, 0.0, -0.8),
+        Vec3(1.0, 0.1, -1.0),
+        Vec3(0.5, -0.3, -0.5),
+        Vec3(0.2, 0.0, -0.8),
         matRG));
     world.add(make_shared<Triangle>( // CW pointing up
-        Point(-0.2, 0.0, -0.8),
-        Point(-1.0, 0.1, -1.0),
-        Point(-0.5, -0.3, -0.5),
+        Vec3(-0.2, 0.0, -0.8),
+        Vec3(-1.0, 0.1, -1.0),
+        Vec3(-0.5, -0.3, -0.5),
         matRG));
     world.add(make_shared<Triangle>( // CW pointing up
-        Point(0.1, -0.5, 0.1),
-        Point(1.0, 0.0, 0.5),
-        Point(0.5, 0.0, 0.1),
+        Vec3(0.1, -0.5, 0.1),
+        Vec3(1.0, 0.0, 0.5),
+        Vec3(0.5, 0.0, 0.1),
         dielectric));
 
     return world;
@@ -91,15 +92,15 @@ HittableList spheresAndTris() {
 std::vector<shared_ptr<Triangle>> triangles(int count) {
     auto tris      = std::vector<shared_ptr<Triangle>>(count);
     double maxPos  = 2.5;
-    double maxEdge = 0.6;
+    double maxEdge = 0.5;
     for (int i = 0; i < count; i++) {
 
-        Point v0 = (Point(randomDouble(), randomDouble(), randomDouble()) - 0.5)
-                 * maxPos;
-        Point v1 = (Point(randomDouble(), randomDouble(), randomDouble()) - 0.5)
-                 * maxEdge;
-        Point v2 = (Point(randomDouble(), randomDouble(), randomDouble()) - 0.5)
-                 * maxEdge;
+        Vec3 v0 = (Vec3(randomDouble(), randomDouble(), randomDouble()) - 0.5)
+                * maxPos * 2.0;
+        Vec3 v1 = (Vec3(randomDouble(), randomDouble(), randomDouble()) - 0.5)
+                * maxEdge * 2.0;
+        Vec3 v2 = (Vec3(randomDouble(), randomDouble(), randomDouble()) - 0.5)
+                * maxEdge * 2.0;
 
         tris[i] = make_shared<Triangle>(
             v0,
@@ -110,72 +111,215 @@ std::vector<shared_ptr<Triangle>> triangles(int count) {
     return tris;
 }
 
+std::vector<shared_ptr<Triangle>> box() {
+    Vec3 center = Vec3(0.0);
+    Vec3 s      = Vec3(2.0);
+    auto mat    = matLambRed;
+
+    Vec3 v[] = {
+        center + Vec3(s.x, s.y, s.z),    // 0
+        center + Vec3(s.x, s.y, -s.z),   // 1
+        center + Vec3(s.x, -s.y, s.z),   // 2
+        center + Vec3(s.x, -s.y, -s.z),  // 3
+        center + Vec3(-s.x, s.y, s.z),   // 4
+        center + Vec3(-s.x, s.y, -s.z),  // 5
+        center + Vec3(-s.x, -s.y, s.z),  // 6
+        center + Vec3(-s.x, -s.y, -s.z), // 7
+    };
+
+    auto tris = std::vector<shared_ptr<Triangle>>({
+        make_shared<Triangle>(v[0], v[1], v[3], mat),
+        make_shared<Triangle>(v[0], v[3], v[2], mat),
+        make_shared<Triangle>(v[0], v[4], v[5], mat),
+        make_shared<Triangle>(v[0], v[5], v[1], mat),
+        make_shared<Triangle>(v[0], v[6], v[4], mat),
+        make_shared<Triangle>(v[0], v[2], v[6], mat),
+        make_shared<Triangle>(v[7], v[5], v[4], mat),
+        make_shared<Triangle>(v[7], v[4], v[6], mat),
+        make_shared<Triangle>(v[7], v[6], v[2], mat),
+        make_shared<Triangle>(v[7], v[2], v[3], mat),
+        make_shared<Triangle>(v[7], v[3], v[1], mat),
+        make_shared<Triangle>(v[7], v[1], v[5], mat),
+    });
+    return tris;
+}
+
+void renderOneBox() {
+    TaskTimer tt;
+    std::ofstream f;
+
+    // New render
+    Camera2 cam;
+    cam.imageWidth      = 300;
+    cam.aspectRatio     = 16.0 / 9.0;
+    cam.samplesPerPixel = 100;
+    cam.lookAt          = Vec3(0.0, 0.0, 3.0);
+    cam.lookFrom        = Vec3(0.0, 0.0, 4.0);
+    cam.defocusAngle    = 0.000001;
+
+    auto tris = box();
+    BVH world(tris);
+    tt.start("BVH render box . . .\n");
+    cam.render(world);
+    tt.stop();
+
+    std::string filename = "runtime/cube.ppm";
+    f.open(filename);
+    if (!f.is_open()) std::cerr << "Failed to open file: " << filename << "\n";
+    cam.img.writeImage(f);
+    f.close();
+}
+
+void renderOneSphere() {
+    TaskTimer tt;
+    std::ofstream f;
+
+    // New render
+    Camera2 cam;
+    cam.imageWidth      = 300;
+    cam.aspectRatio     = 16.0 / 9.0;
+    cam.vfov            = 90;
+    cam.samplesPerPixel = 100;
+    cam.lookAt          = Vec3(0.0, 0.0, 0.0);
+    cam.lookFrom        = Vec3(0.0, 0.0, 2.5);
+    cam.focusDist       = 2.0;
+    cam.defocusAngle    = 0.000001;
+
+    auto mat1 = make_shared<Metal>(Color(0.2, 0.2, 1.0), 0.1);
+
+    auto s = Sphere(Vec3(0.0), 1.0, mat1);
+    tt.start("Render sphere . . .\n");
+    cam.render(s);
+    tt.stop();
+
+    std::string filename = "runtime/sphere.ppm";
+    f.open(filename);
+    if (!f.is_open()) std::cerr << "Failed to open file: " << filename << "\n";
+    cam.img.writeImage(f);
+    f.close();
+}
+
+void renderEarth(double hour = 0.0) {
+    TaskTimer tt;
+    std::ofstream f;
+
+    // New render
+    Camera2 cam;
+    cam.imageWidth      = 800;
+    cam.aspectRatio     = 16.0 / 9.0;
+    cam.vfov            = 90;
+    cam.samplesPerPixel = 100;
+    cam.lookAt          = Vec3(0.0, 0.0, 0.0);
+    cam.lookFrom        = Vec3(0.0, 1.0, -3.0);
+    cam.focusDist       = 1.5;
+    cam.defocusAngle    = 0.0;
+
+    auto imgTexEarth = make_shared<ImageTexture>("resources/earthmap.jpg");
+    auto matEarthmap = make_shared<Lambertian>(imgTexEarth);
+    auto earth       = Sphere(Vec3(0.0, 0.0, 0.0), 2.0, matEarthmap);
+
+    tt.start("Render spheres . . .\n");
+    cam.render(earth);
+    tt.stop();
+
+    std::string filename = "runtime/earth.ppm";
+    f.open(filename);
+    if (!f.is_open()) std::cerr << "Failed to open file: " << filename << "\n";
+    cam.img.writeImage(f);
+    f.close();
+}
+
+void renderTwoSpheres() {
+    TaskTimer tt;
+    std::ofstream f;
+
+    // New render
+    Camera2 cam;
+    cam.imageWidth      = 600;
+    cam.aspectRatio     = 16.0 / 9.0;
+    cam.vfov            = 80;
+    cam.samplesPerPixel = 100;
+    cam.lookAt          = Vec3(-1.25, 0.0, 0.0);
+    cam.lookFrom        = Vec3(-1.25, 2.0, 0.1);
+    cam.focusDist       = 1.5;
+    cam.defocusAngle    = 0.0;
+
+    // STBImage tex("resources/cgaxis_hdri_skies_01_52.jpg");
+    auto imgTex =
+        make_shared<ImageTexture>("resources/cgaxis_hdri_skies_01_52.jpg");
+    auto imgTexEarth = make_shared<ImageTexture>("resources/earthmap.jpg");
+    imgTex->repeatedTexture = 1.0;
+
+    auto checker =
+        make_shared<CheckerTexture>(0.025, Color(1.0), Color(0.0, 0.0, 1.0));
+    auto grad = make_shared<GradientTexture>(
+        Color(1.0, 0.0, 0.0),
+        Color(0.0, 0.0, 1.0),
+        true,
+        false);
+    auto mat1 = make_shared<Metal>(checker, 0.1);
+    // auto mat2 = make_shared<Metal>(Color(0.95, 0.95, 0.95), 0.6);
+    auto mat2 = make_shared<Lambertian>(imgTexEarth);
+    // auto mat2 = make_shared<Lambertian>(grad);
+
+    HittableList world;
+
+    world.add(make_shared<Sphere>(Vec3(1.25, 0.0, 0.0), 1.0, mat1));
+    world.add(make_shared<Sphere>(Vec3(-1.25, 0.0, 0.0), 1.0, mat2));
+    tt.start("Render spheres . . .\n");
+    cam.render(world);
+    tt.stop();
+
+    std::string filename = "runtime/2spheres.ppm";
+    f.open(filename);
+    if (!f.is_open()) std::cerr << "Failed to open file: " << filename << "\n";
+    cam.img.writeImage(f);
+    f.close();
+}
+
+void renderQuads() {
+    TaskTimer tt;
+    std::ofstream f;
+
+    // New render
+    Camera2 cam;
+    cam.imageWidth      = 400;
+    cam.aspectRatio     = 1.0;
+    cam.vfov            = 80;
+    cam.samplesPerPixel = 100;
+    cam.lookAt          = Vec3(0.0, 0.0, 0.0);
+    cam.lookFrom        = Vec3(0.0, 0.0, 9.0);
+    cam.vup             = Vec3(0.0, 1.0, 0.0);
+    cam.focusDist       = 1.5;
+    cam.defocusAngle    = 0.0;
+
+    auto leftRed     = make_shared<Lambertian>(Color(1.0, 0.2, 0.2));
+    auto backGreen   = make_shared<Lambertian>(Color(0.2, 1.0, 0.2));
+    auto rightBlue   = make_shared<Lambertian>(Color(0.2, 0.2, 1.0));
+    auto upperOrange = make_shared<Lambertian>(Color(1.0, 0.5, 0.0));
+    auto lowerTeal   = make_shared<Lambertian>(Color(0.2, 0.8, 0.8));
+
+    HittableList world;
+    world.add(make_shared<Quad>(Vec3(-3, -2, 5), Vec3(0, 0, -4), Vec3(0, 4, 0), leftRed));
+    world.add(make_shared<Quad>(Vec3(-2, -2, 0), Vec3(4, 0, 0), Vec3(0, 4, 0), backGreen));
+    world.add(make_shared<Quad>(Vec3(3, -2, 1), Vec3(0, 0, 4), Vec3(0, 4, 0), rightBlue));
+    world.add(make_shared<Quad>(Vec3(-2, 3, 1), Vec3(4, 0, 0), Vec3(0, 0, 4), upperOrange));
+    world.add(make_shared<Quad>(Vec3(-2, -3, 5), Vec3(4, 0, 0), Vec3(0, 0, -4), lowerTeal));
+    tt.start("Render quads . . .\n");
+    cam.render(world);
+    tt.stop();
+
+    std::string filename = "runtime/quads.ppm";
+    f.open(filename);
+    if (!f.is_open()) std::cerr << "Failed to open file: " << filename << "\n";
+    cam.img.writeImage(f);
+    f.close();
+}
+
 int main(int argc, char* argv[]) {
     for (int i = 1; i < argc; i++) {
         std::cerr << "Unexpected argument: " << argv[i] << std::endl;
     }
-    TaskTimer tt;
-    std::ofstream f;
-    HittableList world;
-
-    // New render
-    Camera2 cam2;
-    cam2.imageWidth      = 400;
-    cam2.aspectRatio     = 16.0 / 9.0;
-    cam2.samplesPerPixel = 150;
-    cam2.lookAt          = Point(0.0, 0.0, -1.0);
-    cam2.lookFrom        = Point(-0.6, 0.5, 0.9);
-
-    // Sheres - - -
-    // world = spheres();
-    // cam2.lookAt          = Point(0.0, 0.0, -1.0);
-    // cam2.lookFrom        = Point(-1.0, 1.0, 0.9);
-
-    // tt.start("Running spheres ray tracing\n");
-    // cam2.render(world);
-    // tt.stop();
-    // f.open("runtime/spheres.ppm");
-    // if (!f.is_open()) std::cerr << "Failed to open file\n";
-    // cam2.img.writeImage(f);
-    // f.close();
-
-    // Triangles - - -
-    cam2.lookAt   = Point(0.0, 0.0, -1.0);
-    cam2.lookFrom = Point(-0.0, 0.0, 2.0);
-    auto tris     = triangles(64);
-    for (auto tri : tris) {
-        world.add(tri);
-    }
-    tt.start("Build BVH . . . ");
-    BVH worldBVH(tris);
-    tt.stop();
-    std::cerr << worldBVH.tree(0);
-    // goto bvh;
-    tt.start("Running ray tracing\n");
-    triIntersections  = 0;
-    aabbIntersections = 0;
-    cam2.render(world);
-    tt.stop();
-    std::cerr << "Intersections: " << triIntersections + aabbIntersections
-              << " (tri: " << triIntersections
-              << ", aabb: " << aabbIntersections << ")";
-    f.open("runtime/trianglesNaive.ppm");
-    if (!f.is_open()) std::cerr << "Failed to open file\n";
-    cam2.img.writeImage(f);
-    f.close();
-
-bvh:
-    // cam2.imageWidth = 100;
-    tt.start("Running BVH ray tracing\n");
-    triIntersections  = 0;
-    aabbIntersections = 0;
-    cam2.render(worldBVH);
-    tt.stop();
-    std::cerr << "Intersections: " << triIntersections + aabbIntersections
-              << " (tri: " << triIntersections
-              << ", aabb: " << aabbIntersections << ")";
-    f.open("runtime/trianglesBVH.ppm");
-    if (!f.is_open()) std::cerr << "Failed to open file\n";
-    cam2.img.writeImage(f);
-    f.close();
+    // renderEarth();
+    renderQuads();
 }
---
+++ src/shape/quad.hpp
@@ -0,0 +1,73 @@
+/// @file shape/quad.hpp
+/// Implicit shape quadrilateral (technically a parallelogram).
+#pragma once
+
+#include "hittable.hpp"
+#include "material.hpp"
+#include "ray.hpp"
+#include "rtweekend.hpp"
+
+class Quad : public Hittable {
+public:
+    Quad(Vec3 q, Vec3 u, Vec3 v, shared_ptr<Material> m)
+        : q(q)
+        , uEdge(u)
+        , vEdge(v)
+        , mat(m) { }
+
+    bool hit(const Ray& ray, double tMin, double tMax, HitRecord& rec)
+        const override {
+        // https://raytracing.github.io/books/RayTracingTheNextWeek.html
+        // #quadrilaterals/ray-planeintersection
+        auto n      = glm::cross(uEdge, vEdge);
+        auto normal = glm::normalize(n);
+        auto d      = glm::dot(normal, q);
+
+        auto denom = glm::dot(normal, ray.direction);
+
+        // Ray is parallel to the plane
+        if (std::fabs(denom) < nearZero) return false;
+
+        auto t = (d - glm::dot(normal, ray.origin)) / denom;
+        // Hit point parameter is outside the ray interval
+        if (t < tMin || tMax < t) return false;
+
+        // Determine if the point lies within the planar shape using its plane
+        // coordinates.
+        auto isect          = ray.at(t);
+        Vec3 planarHitptVec = isect - q;
+        auto w              = n / glm::dot(n, n);
+        auto alpha          = glm::dot(w, glm::cross(planarHitptVec, vEdge));
+        auto beta           = glm::dot(w, glm::cross(uEdge, planarHitptVec));
+
+        if (!isInterior(alpha, beta, rec)) return false;
+
+        rec.t   = t;
+        rec.p   = isect;
+        rec.mat = mat;
+        // TODO: Set u, v
+        rec.setFaceNormal(ray, normal);
+        return true;
+    }
+
+    Vec3 centroid() const { return q + (uEdge + vEdge) / 2.0; }
+
+private:
+    /// @brief Given the hit point in plane coordinates, return false if it is
+    /// outside the primitive, otherwise set the hit record UV coordinates and
+    /// return true.
+    /// @param a
+    /// @param b
+    /// @param rec
+    /// @return
+    bool isInterior(double a, double b, HitRecord& rec) const {
+        if ((a < 0) || (1 < a) || (b < 0) || (1 < b)) return false;
+
+        rec.u = a;
+        rec.v = b;
+        return true;
+    }
+    Vec3 q;
+    Vec3 uEdge, vEdge;
+    shared_ptr<Material> mat;
+};
