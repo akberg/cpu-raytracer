bb9ad34bf8e4c189f3b7db26c8a3e5e56fcab986
Andreas K. Berg <andreaskb98@gmail.com>
2023-11-16 15:06:30 +0100

    Tiling in renderer

--- src/camera.cpp
+++ src/camera.cpp
@@ -4,24 +4,37 @@
 void Camera::render(const Hittable& world) {
     initialize();
     double sampleCoefficient = 1.0 / static_cast<double>(samplesPerPixel);
-    for (int j = 0; j < img.height; j++) {
-        for (int i = 0; i < img.width; i++) {
-            Color pxColor(0.0);
-            for (int s = 0; s < samplesPerPixel; s++) {
-                auto u = (i + randomDouble()) / (img.width - 1);
-                auto v = (j + randomDouble()) / (img.height - 1);
-                auto r = getRay(i, j);
-                pxColor += rayColor(r, world, maxDepth);
-                // if (j % 40 == 0 && i % 20 == 0 && !s)
-                //     std::cerr << "(" << i << "," << j << ")" << r << " :
-                //     "
-                //               << pxColor << std::endl;
+    // Tiling can greatly improve render time, but will be scene dependent and completely irrelevant
+    // for BVH structures. Tesing uniti.tri (samples=16,depth=8, width=400) on Arm remote machine,
+    // tile size:
+    // - 1: 4272ms
+    // - 4: 751ms
+    // - 8: 354ms
+    // - 16: 333ms
+    int tileSize             = 16;
+    for (int j = 0; j < img.height; j += tileSize) {
+        for (int i = 0; i < img.width; i += tileSize) {
+            for (int jt = 0; jt < tileSize && j + jt < img.height; jt++) {
+                for (int it = 0; it < tileSize && i + it < img.width; it++) {
+                    Color pxColor(0.0);
+                    int x = i + it;
+                    int y = j + jt;
+                    for (int s = 0; s < samplesPerPixel; s++) {
+                        auto u = (x + randomDouble()) / (img.width - 1);
+                        auto v = (y + randomDouble()) / (img.height - 1);
+                        auto r = getRay(x, y);
+                        pxColor += rayColor(r, world, maxDepth);
+                        // if (j % 40 == 0 && i % 20 == 0 && !s)
+                        //     std::cerr << "(" << i << "," << j << ")" << r << " :
+                        //     "
+                        //               << pxColor << std::endl;
+                    }
+                    img.setPixel(x, y, pxColor * sampleCoefficient);
+                }
             }
-            img.setPixel(i, j, pxColor * sampleCoefficient);
         }
         if (j % (img.height / 10) == 0) {
-            std::cerr << "Ray tracing progress: "
-                        << j * 10 / (img.height / 10) << "%\n";
+            std::cerr << "Ray tracing progress: " << j * 10 / (img.height / 10) << "%\n";
         }
     }
 }
@@ -32,11 +45,10 @@ void Camera::render(const Hittable& world) {
 /// @param v Vertical position
 /// @param exact Set true to return exact direction instead of sampling
 /// within pixel square
-Ray Camera::getRay(double u, double v, bool exact = false) const {
+Ray Camera::getRay(double u, double v, bool exact) const {
     auto pixelCenter = pixel00Loc + (u * uPixelDelta) + (v * vPixelDelta);
-    auto pixelSample =
-        pixelCenter + (exact ? Vec3(0.0) : pixelSampleSquare());
-    auto rOrigin = (defocusAngle <= 0) ? origin : defocusDiskSample();
+    auto pixelSample = pixelCenter + (exact ? Vec3(0.0) : pixelSampleSquare());
+    auto rOrigin     = (defocusAngle <= 0) ? origin : defocusDiskSample();
     return Ray(rOrigin, pixelSample - rOrigin);
 }
 
@@ -46,15 +58,13 @@ void Camera::initialize() {
 
     // Resize image buffer
     if (img.width != imageWidth || img.height != imageHeight) {
-        std::cerr << "Resize image (" << imageWidth << "," << imageHeight
-                    << ")\n";
+        std::cerr << "Resize image (" << imageWidth << "," << imageHeight << ")\n";
         img.resize(imageWidth, imageHeight);
     }
 
     origin = lookFrom; // Why would we need to duplicate this???
 
-    std::cerr << "Camera origin: " << origin << " look at: " << lookAt
-                << "\n";
+    std::cerr << "Camera origin: " << origin << " look at: " << lookAt << "\n";
 
     // Calculate the u, v, w unit basis vectos for the camera coordinate
     // frame.
@@ -67,8 +77,7 @@ void Camera::initialize() {
     auto theta          = degreesToRadians(vfov);
     auto h              = tan(theta / 2);
     auto viewportHeight = 2 * h * focusDist;
-    auto viewportWidth =
-        viewportHeight * (static_cast<double>(imageWidth) / imageHeight);
+    auto viewportWidth  = viewportHeight * (static_cast<double>(imageWidth) / imageHeight);
 
     // Calculate the vectors across the horizontal and down the vertical
     // viewport edges. Edges go from lower left corner.
@@ -83,24 +92,19 @@ void Camera::initialize() {
     // Calculate the location of the upper left pixel.
     // sub focalLength, looking towards negative Z.
     // Place location in the middle of the pixel's covered area.
-    viewportLowerLeft =
-        origin - (focusDist * w) - uViewport / 2.0 - vViewport / 2.0;
-    pixel00Loc = viewportLowerLeft + 0.5 * (uPixelDelta + vPixelDelta);
+    viewportLowerLeft = origin - (focusDist * w) - uViewport / 2.0 - vViewport / 2.0;
+    pixel00Loc        = viewportLowerLeft + 0.5 * (uPixelDelta + vPixelDelta);
 
     // Calculate camera defocus disk basis vectors.
-    auto defocusRadius =
-        focusDist * glm::tan(degreesToRadians(defocusAngle / 2.0));
-    defocusDisk_u = u * defocusRadius;
-    defocusDisk_v = v * defocusRadius;
+    auto defocusRadius = focusDist * glm::tan(degreesToRadians(defocusAngle / 2.0));
+    defocusDisk_u      = u * defocusRadius;
+    defocusDisk_v      = v * defocusRadius;
 
-    std::cerr << "Pixel (0,0) location: " << pixel00Loc
-                << "\nuViewport: " << uViewport
-                << " uPixelDelta: " << uPixelDelta
-                << "\nvViewport: " << vViewport
-                << " vPixelDelta: " << vPixelDelta
-                << "\nu defocus disk: " << defocusDisk_u
-                << " v defocus disk: " << defocusDisk_v
-                << "\ndefocus radius: " << defocusRadius << "\n";
+    std::cerr << "Pixel (0,0) location: " << pixel00Loc << "\nuViewport: " << uViewport
+              << " uPixelDelta: " << uPixelDelta << "\nvViewport: " << vViewport
+              << " vPixelDelta: " << vPixelDelta << "\nu defocus disk: " << defocusDisk_u
+              << " v defocus disk: " << defocusDisk_v << "\ndefocus radius: " << defocusRadius
+              << "\n";
 }
 
 /// @brief Returns a random point in the square surrounding a pixel at the
